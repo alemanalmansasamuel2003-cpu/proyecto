037e761435c3c7a0a64961e6ef9b8739067ba6ad
Samuel Alemán <aleman.almansa.samuel.2003@gmail.com>
2025-10-13 11:49:20 -0600

    Parte 1 - proyecto universitario

---
+++ vistas/vista_superior.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=" spanish">
+
+<head>
+<meta charset="utf-8">
+<met name="proyecto" content="proyecto universitario">
+<title>proyecto</title>
+<link rel="stylesheet" href="./css/main.css">
+
+<body>
+    <h1>Hola, este es mi proyecto universitario</h1>
+    <p>Bienvenido a la página.</p>
+</body>
+
+</head>
